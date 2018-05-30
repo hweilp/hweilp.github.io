039c78b4a693c2e0c4b383bd1371cdcc9670f2e8
hw <18379036722@163.com>
2018-05-30 18:20:27 +0800

    时间

--- static/js/index.js
+++ static/js/index.js
@@ -58,6 +58,33 @@ $(document).ready(function () {
 
 		},
 
+		LatestRelease : function(obj){
+			$.ajax({
+				type:'get',
+				url :'static/json/lastest_release.json',
+				success : function (res) {
+					var result = res.list;
+					var Html = '';
+					for ( var i = 0 ; i < result.length; i ++ ){
+						Html += '<li>' +
+							'<a class="clear-fix" target="_blank" href="'+ result[i].url +'">' +
+							'<img class="fl" src="'+ result[i].imgUrl +'" alt="">' +
+							'<div class="text fr">' +
+							'<h3>'+ result[i].title +'</h3>' +
+							'<p class="desc">'+ result[i].desc +'</p>' +
+							'<p class="date">'+ result[i].date +'</p>' +
+							'</div>' +
+							'</a>' +
+							'</li>'
+					}
+					obj.html(Html);
+				},
+				error : function (err) {
+
+				}
+			})
+		},
+
 		FriendshipLink : function (obj) {
 			$.ajax({
 				type:'get',
@@ -74,7 +101,9 @@ $(document).ready(function () {
 			})
 		},
 
-		TodayTime : function (obj) {
+		TodayTime : function (obj,type) {
+			var weekdays = ["日","一","二","三","四","五","六"];
+
 			var nowDate = {
 				year : '',
 				month : '',
@@ -86,13 +115,25 @@ $(document).ready(function () {
 			};
 
 			setInterval(function () {
-				nowDate.y = new Date().getFullYear();
-				nowDate.m = new Date().getMonth()+1;
-				nowDate.d = new Date().getDate();
-				nowDate.h = new Date().getHours();
-				nowDate.min = new Date().getMinutes();
-				nowDate.sec = new Date().getSeconds();
+				nowDate.year = new Date().getFullYear();
+				nowDate.month = new Date().getMonth()+1  > 10 ? "0" + new Date().getMonth()+1: new Date().getMonth()+1;
+				nowDate.day = Number(new Date().getDate()) < 10 ? "0" + new Date().getDate() : new Date().getDate();
+				nowDate.hours = Number(new Date().getHours()) < 10 ? "0" + new Date().getHours() : new Date().getHours();
+				nowDate.min = Number(new Date().getMinutes()) < 10 ? "0" + new Date().getMinutes() : new Date().getMinutes();
+				nowDate.sec = Number(new Date().getSeconds()) < 10 ? "0" + new Date().getSeconds() : new Date().getSeconds();
 				nowDate.week = new Date().getDay();
+
+				var Html = '';
+				if(type == 'date'){
+					Html = nowDate.year + '年' + nowDate.month + '月' + nowDate.day + '日'
+				}
+				if(type == 'time'){
+					Html = nowDate.hours + '时' + nowDate.min + '分' + nowDate.sec + '秒'
+				}
+				if(type == 'week'){
+					Html = '星期' + weekdays[nowDate.week]
+				}
+				obj.html(Html);
 				return nowDate;
 			},1000)
 		}
@@ -104,9 +145,16 @@ $(document).ready(function () {
 	// 个人推荐
 	new Index.Recommend($('#recommend'));
 
+	// 最新发布
+	new Index.LatestRelease($('#latest-release'));
+
 	// 友情链接
 	new Index.FriendshipLink($('#link'));
 
+	// 日期
+	new Index.TodayTime($('#date'),'date');
 	// 今日时间
-	new Index.TodayTime($('#date'));
+	new Index.TodayTime($('#time'),'time');
+	// 今日星期
+	new Index.TodayTime($('#week'),'week');
 });
