c4a1d016e2c09bc71b295ea401c7916de1ca5abe
hw <18379036722@163.com>
2018-09-11 10:51:11 +0800

    修改图片上传

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,19 @@
 var apiConfig = require('../controller/apiConfig');
 var Auth = function(req,res,next){
-  next();
-  // if (!req.session.SESSION_ID) {
-  //   if (req.method.toLowerCase() === 'get') {
-  //     if(req.url.indexOf('api') != -1){
-  //       apiConfig.error(res,1001);
-  //     }else {
-  //       req.session.PREVENT_PATH = req.url;
-  //       res.redirect('/login');
-  //     }
-  //   }else {
-  //     apiConfig.error(res,1001)
-  //   }
-  // } else {
-  //   next();
-  // }
+  if (!req.session.SESSION_ID) {
+    if (req.method.toLowerCase() === 'get') {
+      if(req.url.indexOf('api') != -1){
+        apiConfig.error(res,1001);
+      }else {
+        req.session.PREVENT_PATH = req.url;
+        res.redirect('/login');
+      }
+    }else {
+      apiConfig.error(res,1001)
+    }
+  } else {
+    next();
+  }
 
 };
 
