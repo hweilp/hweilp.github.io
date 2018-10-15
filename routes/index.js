fd203554366f3ea6c8ac889748e127df765c574c
hw <18379036722@163.com>
2018-10-15 14:49:07 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.use(function (req,res,next){
 
 //设置跨域访问
 router.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "X-Requested-With")
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
     res.header("X-Powered-By",' 3.2.1')
