b2099f915699a2d171cb8936d83cfd54723b7a64
hw <18379036722@163.com>
2019-09-23 21:03:41 +0800

    birthday fix

--- js/login.js
+++ js/login.js
@@ -8,7 +8,7 @@ $('#login-button').click(function (event) {
 		$('.wrapper').addClass('form-success');
 		// requestFullScreen();
 		setTimeout(function () {
-			location.href = "BirthdayCake.html";
+			location.href = "cake.html";
 		}, 2000);
 	} else {
 		alert("Wrong Password");
